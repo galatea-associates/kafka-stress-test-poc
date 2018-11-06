db0e99e7d0a66728c63aa8c3728b77a0e2c4481c
Danny Sortino <sortino@galatea-associates.com>
2018-11-06 14:39:07 +0000

    Added safety to avoid crashing on division by 0

--- SimpleProducer.py
+++ SimpleProducer.py
@@ -54,6 +54,8 @@ def cleanup_producer(producer):
     producer.close()
 
 def produce_output(dict_key, output_time):
+    if len(shared_dict[dict_key]) == 0:
+        return
     print(dict_key + " - Mean: " + str(sum(shared_dict[dict_key]) / len(shared_dict[dict_key])) + " Max: " + str(max(shared_dict[dict_key])) + " Min: " + str(min(shared_dict[dict_key])) ) 
     with open("output-send-" + str(int(output_time)) + ".csv", 'a', newline='') as output_file:
         wr = csv.writer(output_file, quoting=csv.QUOTE_ALL)
