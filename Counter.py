177c3f15a34e21945e95fa8fa94a0f7a16112f46
Danny Sortino <sortino@galatea-associates.com>
2018-11-06 17:48:07 +0000

    Added fix for shared limit val

--- Counter.py
+++ Counter.py
@@ -1,9 +1,9 @@
 from multiprocessing import Lock, Value
 
 class Counter(object):
-    def __init__(self, init_val=0, limit=0):
+    def __init__(self, init_val=0, limit_val=0):
         self.val = Value('i', init_val)
-        self.limit = Value('i', limit)
+        self.limit = limit_val
         self.lock = Lock()
 
     def increment(self):
@@ -20,7 +20,10 @@ class Counter(object):
 
     def check_value_and_increment(self):
         with self.lock:
-            value_beneath_limit = self.val.value < self.limit.value
+            value_beneath_limit = self.val.value < self.limit
             if value_beneath_limit:
                 self.val.value += 1
             return value_beneath_limit
+
+    def get_limit(self):
+        return self.limit
