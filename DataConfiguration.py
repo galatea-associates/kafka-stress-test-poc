a6dbd6064194a3da42bf73f76ec6a6095773e61c
Florian Emile <emile@galatea-associates.com>
2018-11-12 10:15:40 +0000

    Make changes to take in account the fact that the methods are static for now

--- DataConfiguration.py
+++ DataConfiguration.py
@@ -1,6 +1,5 @@
 from DataGenerator import DataGenerator
 
-data_gen = DataGenerator()
 # This file will contrain the core configuration for running the consumer and producer.
 # It will also contain the functions needed to generate data for the different topics.
 
@@ -11,7 +10,7 @@ configuration = {
             "limit_val" : 40000
         },
         "Avro Schema": "prices.avsc",
-        "Value" : data_gen.generate_price_entity,
+        "Value" : DataGenerator.generate_price_entity,
         "Number of Processes" : 11,
         "Time Interval" : 1.0      
     },
@@ -21,7 +20,7 @@ configuration = {
             "limit_val" : 20000
         },
         "Avro Schema": "positions.avsc",
-        "Value" : data_gen.generate_position_entity,
+        "Value" : DataGenerator.generate_position_entity,
         "Number of Processes" : 4,
         "Time Interval" : 1.0      
     },
@@ -31,7 +30,7 @@ configuration = {
             "limit_val" : 100
         },
         "Avro Schema": "instrument_reference_data.avsc",
-        "Value" : data_gen.generate_inst_ref_entity,
+        "Value" : DataGenerator.generate_inst_ref_entity,
         "Number of Processes" : 1,
         "Time Interval" : 60.0      
     },
